5a45f3a126d358b4e12e536c79a4f82857ab5392
yuanzhenkun <yuanzhenkun@baidu.com>
2021-03-08 09:57:26 +0800

    update cube geo

--- src/cube-geometry/01.js
+++ src/cube-geometry/01.js
@@ -4,8 +4,20 @@ const V_SHADER_DATA = `
 const F_SHADER_DATA = `
 `;
 
+const vertices = new Float32Array([
+
+]);
+
+const uv = new Float32Array([
+
+]);
+
+const index = new Float32Array([
+
+]);
+
 function start() {
-    
+
 }
 
 start();
