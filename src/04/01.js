81a5d2e6624b7801578c3b50ec2bf69307ba98f6
yuanzhenkun <yuanzhenkun@baidu.com>
2021-03-31 23:46:16 +0800

    something useless

--- src/04/01.js
+++ src/04/01.js
@@ -1,5 +1,6 @@
 console.log('create');
 let canvas = document.getElementById('canvas');
 let gl = canvas.getContext('webgl');
-gl.clearColor(1, 0, 0, 1);
+
+gl.clearColor(1, 1, 0, 1);
 gl.clear(gl.COLOR_BUFFER_BIT);
