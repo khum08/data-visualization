81d229fc67461451ed9d3a00323f49fb80c411b7
yuanzhenkun <yuanzhenkun@baidu.com>
2021-04-08 23:23:31 +0800

    template

--- src/cube-geometry/05.js
+++ src/cube-geometry/05.js
@@ -6,3 +6,33 @@ var gl = canvas.getContext('webgl', {
 
 gl.clearColor(1.0, 0, 0, 1.0);
 gl.clear(gl.COLOR_BUFFER_BIT);
+
+const V_SHADER_DATA = `
+attribute vec3 aPosition;
+
+uniform mat4 uModelMat;
+uniform mat4 uViewMat;
+uniform mat4 uProjMat;
+void main() {
+    gl_Position = aPosition;
+}
+`;
+
+const F_SHADER_DATA = `
+#ifdef GL_ES
+precision mediump float;
+#endif
+void main() {
+    gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);
+}
+`;
+
+function start() {
+    
+}
+
+function update() {
+    requestAnimationFrame(update);
+}
+
+start();
